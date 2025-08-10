f41066540e551df8a31831787bfa8281d36816c0
PekiPC <v.a.r.d.games123@gmail.com>
2025-08-10 16:17:17 +0200

    Finished AutoMapper and Dtos

--- Backend/Dto/Dtos.cs
+++ Backend/Dto/Dtos.cs
@@ -25,3 +25,78 @@ public class UpdateCommentDto
 {
     public string? Content { get; set; }
 }
+
+
+public class UserDto
+{
+    public int Id { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? RoleName { get; set; }
+
+    public List<Guid>? ReportsId { get; set; }
+
+    public List<Guid>? FollowingId { get; set; }
+
+    public List<Guid>? CommentsId { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+}
+
+
+public class MediaDto
+{
+    public Guid MediaId { get; set; }
+
+    public string? Url { get; set; }
+
+    public Guid? ReportId { get; set; }
+}
+
+public class CommentDto
+{
+    public Guid Id { get; set; }
+
+    public Guid? ReportId { get; set; }
+
+    public string? Content { get; set; }
+
+    public string? Username { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTimeOffset DateOfComment { get; set; }
+}
+
+
+public class ReportDto
+{
+    public Guid Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTimeOffset DateOfPost { get; set; }
+
+    public string? Username { get; set; }
+
+    public List<Guid>? CommentIds { get; set; }
+
+    public List<Guid>? MediaIds { get; set; }
+
+    public string? RegionName { get; set; }
+
+    public string? SeverityLevel { get; set; }
+
+    public string? ResolutionStatus { get; set; }
+
+    public Guid? PinId { get; set; }
+
+    public List<string>? TagNames { get; set; }
+
+    public List<string>? FollowerUsernames { get; set; }
+}
--- Backend/Dto/Mapper.cs
+++ Backend/Dto/Mapper.cs
@@ -1,9 +1,37 @@
 using AutoMapper;
+using Backend.Dtos;
 
 public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        
+        CreateMap<User, UserDto>()
+            .ForMember(a => a.CommentsId, b => b.MapFrom(c => c.Comments.Select(d => d.Id).ToList()))
+            .ForMember(a => a.ReportsId, b => b.MapFrom(c => c.Reports.Select(d => d.Id).ToList()))
+            .ForMember(a => a.FollowingId, b => b.MapFrom(c => c.Following.Select(d => d.Id).ToList()))
+            .ForMember(a => a.RoleName, b => b.MapFrom(c => c.Role != null ? c.Role.Name : ""))
+            .ForMember(a => a.CreatedAt, b => b.MapFrom(c => c.CreatedAt));
+
+
+        CreateMap<Media, MediaDto>()
+            .ForMember(a => a.ReportId, b => b.MapFrom(c => c.Report != null ? c.Report.Id : (Guid?)null));
+
+
+        CreateMap<Comment, CommentDto>()
+            .ForMember(a => a.ReportId, b => b.MapFrom(c => c.Report != null ? c.Report.Id : (Guid?)null))
+            .ForMember(a => a.Username, b => b.MapFrom(c => c.User != null ? c.User.Username : ""));
+
+
+
+        CreateMap<Report, ReportDto>()
+            .ForMember(a => a.CommentIds, b => b.MapFrom(c => c.Comments != null ? c.Comments.Select(d => d.Id).ToList() : new List<Guid>()))
+            .ForMember(a => a.MediaIds, b => b.MapFrom(c => c.Media != null ? c.Media.Select(d => d.MediaId).ToList() : new List<Guid>()))
+            .ForMember(a => a.Username, b => b.MapFrom(c => c.User != null ? c.User.Username : ""))
+            .ForMember(a => a.TagNames, b => b.MapFrom(c => c.Tags.Select(d => d.Name).ToList()))
+            .ForMember(a => a.RegionName, b => b.MapFrom(c => c.Region != null ? c.Region.Name : ""))
+            .ForMember(a => a.ResolutionStatus, b => b.MapFrom(c => c.ResolutionStatus != null ? c.ResolutionStatus.Status : ""))
+            .ForMember(a => a.SeverityLevel, b => b.MapFrom(c => c.Severity != null ? c.Severity.Level : ""))
+            .ForMember(a => a.PinId, b => b.MapFrom(c => c.Pin != null ? c.Pin.Id : (Guid?)null))
+            .ForMember(a => a.FollowerUsernames, b => b.MapFrom(c => c.Followers != null ? c.Followers.Select(d => d.Username).ToList() : new List<string>()));
     }
 }
