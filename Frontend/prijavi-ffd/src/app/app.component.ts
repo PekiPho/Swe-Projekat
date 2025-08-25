98388a85220d1fa647d5be03dabce8ca1a603bfd
f1cejfvr <filipjovanovic107@gmail.com>
2025-08-25 16:02:00 +0200

    dodato da ostaje user

--- Frontend/prijavi-ffd/src/app/app.component.ts
+++ Frontend/prijavi-ffd/src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,RouterModule, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'prijavi-ffd';
 
+  constructor(private userService:UserService){}
+
+  ngOnInit() {
+  this.userService.getEntry().subscribe({
+      next:(data)=>{
+        var user = JSON.parse(data);
+        this.userService.setUser(user);
+        //console.log(user);
+      },
+      error:(err)=>{
+        
+      }
+    });
+}
 }
