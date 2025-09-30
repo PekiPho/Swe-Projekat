0f83545f9d93519b6851ccec520240ca625b26b6
PekiPC <v.a.r.d.games123@gmail.com>
2025-09-30 11:54:20 +0200

    Mapa inicijalizovana

--- Frontend/prijavi-ffd/src/app/pages/map-page/map-page.component.ts
+++ Frontend/prijavi-ffd/src/app/pages/map-page/map-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map-page',
@@ -7,6 +8,28 @@ import { NavbarComponent } from '../../navbar/navbar.component';
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.scss'
 })
-export class MapPageComponent {
+export class MapPageComponent implements AfterViewInit{
 
+  private map!: L.Map;
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap():void{
+
+    const srbijaCentar: L.LatLngExpression = [44.5,21.0];
+    const zoom=7.5;
+
+
+    this.map=L.map('map',{
+      center:srbijaCentar,
+      zoom:zoom
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      attribution: '&copy; OpenStreetMap contributors',
+      maxZoom:19
+    }).addTo(this.map);
+  }
 }
